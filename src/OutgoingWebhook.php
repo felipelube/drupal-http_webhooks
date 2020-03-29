722418cf82c5b1af61008be4caf1a968d4444ecf
Felipe <felipe.lubra@gmail.com>
2020-03-29 18:58:26 -0300

    feat: basic functionality with all entity types and primary events

--- src/OutgoingWebhook.php
+++ src/OutgoingWebhook.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace Drupal\http_webhooks;
-use GuzzleHttp\ClientInterface;
+
 use Drupal\Component\Serialization\SerializationInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Request;
 
 /**
  * Class OutgoingWebhook.
  */
 class OutgoingWebhook {
+  const EVENT_CREATED = "created";
+  const EVENT_UPDATED = "updated";
+  const EVENT_DELETED = "deleted";
 
   /**
    * GuzzleHttp\ClientInterface definition.
@@ -41,6 +49,32 @@ class OutgoingWebhook {
     $this->serializationJson = $serialization_json;
     $this->configFactory = $config_factory;
   }
+
+  public function handle_event(EntityInterface $entity, $event) {
+    // TODO: only post for entities and events allowed in configuration
+    $this->post();
+  }
+
+  public function post() {
+    $config = $this->configFactory->get('http_webhooks.outgoing_config');
+    $secret = $config->get('http_webhooks.outgoing.secret');
+    $url = $config->get('http_webhooks.outgoing.url');
+    if (empty($secret) || empty($url)) {
+      // TODO: log a error message: these configuration are necessary,
+      return;
+    }
+
+    try {
+      $response = $this->httpClient->request('POST', $url, [
+        'json' => ['secret'=> $secret]
+      ]);
+    } catch(RequestException $e) {
+      // TODO: log a error message: the request failed
+      return;
+    }
+    $body = $response->getBody();
+    // TODO: log a success message with the response payload
+
   }
 
 }
