53048f451c8418225d3342848bfc67286d7460fe
Felipe Lübe de Bragança <felipe.lubra@gmail.com>
2020-03-30 13:40:00 -0300

    feat: implement core functionality (#8)
    * chore: add and use config.factory as a service dependency
    
    * feat: basic functionality with all entity types and primary events
    
    * feat: allow the selection of the entities and events to fire the webhook
    - renaming of event constants
    - valid events as a constant inside OutgoingWebhook service class
    - avoid instantiating config objects multiple times, cache once the
    config object inside OutgoingWebhook constructor
    - reorder secret field inside OutgoingWebhookConfigForm
    - serialize selected events as a array in the configuration

--- src/Form/OutgoingWebhookConfigForm.php
+++ src/Form/OutgoingWebhookConfigForm.php
@@ -4,6 +4,7 @@ namespace Drupal\http_webhooks\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\http_webhooks\OutgoingWebhook;
 
 /**
  * Class OutgoingWebhookConfigForm.
@@ -26,11 +27,21 @@ class OutgoingWebhookConfigForm extends ConfigFormBase {
     return 'outgoing_webhook_config_form';
   }
 
+  protected function getEventOptions() {
+    return OutgoingWebhook::VALID_EVENTS;
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('http_webhooks.outgoing_config');
+    $form['url'] = [
+      '#type' => 'url',
+      '#title' => $this->t('URL of the webhook'),
+      '#description' => $this->t('The URL to make the POST request'),
+      '#default_value' => $config->get('http_webhooks.outgoing.url'),
+    ];
     $form['secret'] = [
       '#type' => 'password',
       '#title' => $this->t('Secret'),
@@ -39,13 +50,35 @@ class OutgoingWebhookConfigForm extends ConfigFormBase {
       '#size' => 64,
       '#default_value' => $config->get('http_webhooks.outgoing.secret'),
     ];
-    $form['url'] = [
-      '#type' => 'url',
-      '#title' => $this->t('URL of the webhook'),
-      '#description' => $this->t('The URL to make the POST request'),
-      '#default_value' => $config->get('http_webhooks.outgoing.url'),
+    $form['events'] = [
+      '#type' => 'tableselect',
+      '#header' => array('type' => 'Entity Type' , 'event' => 'Event'),
+      '#description' => $this->t("The events that will trigger this webhook."),
+      '#options' => $this->getEventOptions(),
     ];
+    $form['events']['#default_value'] = ($config->isNew()
+      ? []
+      : $this->deSerializeEvents($config->get('http_webhooks.outgoing.events'))
+    );
     return parent::buildForm($form, $form_state);
+  }
+
+  protected function serializeEvents($events) {
+    return array_filter(array_values($events));
+  }
+
+  protected function deSerializeEvents($events) {
+    $options = $this->getEventOptions();
+    $result = \array_reduce(array_keys($options), function($carry, $key) use($events) {
+      if (in_array($key, $events)) {
+        $carry[$key] = $key;
+      } else {
+        $carry[$key] = 0;
+      }
+      return $carry;
+    }, []);
+    $result = (object) $result;
+    return $result;
   }
 
   /**
@@ -54,9 +87,12 @@ class OutgoingWebhookConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
+    $serializedEvents = $this->serializeEvents($form_state->getValue('events'));
+
     $this->config('http_webhooks.outgoing_config')
       ->set('http_webhooks.outgoing.secret', $form_state->getValue('secret'))
       ->set('http_webhooks.outgoing.url', $form_state->getValue('url'))
+      ->set('http_webhooks.outgoing.events', $serializedEvents)
       ->save();
   }
 
--- src/OutgoingWebhook.php
+++ src/OutgoingWebhook.php
@@ -1,13 +1,34 @@
 <?php
 
 namespace Drupal\http_webhooks;
-use GuzzleHttp\ClientInterface;
+
 use Drupal\Component\Serialization\SerializationInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Request;
 
 /**
  * Class OutgoingWebhook.
  */
 class OutgoingWebhook {
+  const EVENT_CREATE = "create";
+  const EVENT_UPDATE = "update";
+  const EVENT_DELETE = "delete";
+
+  const VALID_EVENTS = [
+    'entity:user:create' => ['type' => 'user' , 'event' => 'create'],
+    'entity:user:update' => ['type' => 'user' , 'event' => 'update'],
+    'entity:user:delete' => ['type' => 'user' , 'event' => 'delete'],
+    'entity:node:create' => ['type' => 'node' , 'event' => 'create'],
+    'entity:node:update' => ['type' => 'node' , 'event' => 'update'],
+    'entity:node:delete' => ['type' => 'node' , 'event' => 'delete'],
+    'entity:comment:create' => ['type' => 'node' , 'event' => 'create'],
+    'entity:node:update' => ['type' => 'node' , 'event' => 'update'],
+    'entity:node:delete' => ['type' => 'node' , 'event' => 'delete'],
+  ];
 
   /**
    * GuzzleHttp\ClientInterface definition.
@@ -24,11 +45,53 @@ class OutgoingWebhook {
   protected $serializationJson;
 
   /**
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
    * Constructs a new OutgoingWebhook object.
    */
-  public function __construct(ClientInterface $http_client, SerializationInterface $serialization_json) {
+  public function __construct(
+    ClientInterface $http_client,
+    SerializationInterface $serialization_json,
+    ConfigFactoryInterface $config_factory
+  ) {
     $this->httpClient = $http_client;
     $this->serializationJson = $serialization_json;
+    $this->config = $config_factory->get('http_webhooks.outgoing_config');
+  }
+
+  public function handle_event(EntityInterface $entity, $event) {
+    $type = $entity->getEntityTypeId();
+    $eventString = "entity:$type:$event";
+    $allowed_events = $this->config->get("http_webhooks.outgoing.events");
+
+    // only post for entities and events allowed in the configuration
+    if (in_array($eventString, $allowed_events)) {
+      $this->post();
+    };
+  }
+
+  public function post() {
+    $secret = $this->config->get('http_webhooks.outgoing.secret');
+    $url = $this->config->get('http_webhooks.outgoing.url');
+    if (empty($secret) || empty($url)) {
+      // TODO: log a error message: these configuration are necessary,
+      return;
+    }
+
+    try {
+      $response = $this->httpClient->request('POST', $url, [
+        'json' => ['secret'=> $secret]
+      ]);
+    } catch(RequestException $e) {
+      // TODO: log a error message: the request failed
+      return;
+    }
+    $body = $response->getBody();
+    // TODO: log a success message with the response payload
+
   }
 
 }
